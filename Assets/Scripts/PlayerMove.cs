e4e050111658a93d35cb0b883756f33b17985885
YoungKwang-Kim <kyk01910691@gmail.com>
2023-12-01 10:44:19 +0900

    이동방법과 점프 수정
    Character Controller 컴포넌트를 이용한 이동 구현.
    isJumping 이라는 변수를 통해 점프의 중복 방지.

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,11 +6,21 @@ public class PlayerMove : MonoBehaviour
 {
     // 이동속도
     public float moveSpeed = 7f;
+    // 캐릭터 컨트롤러 컴포넌트 변수
+    CharacterController cc;
+    // 중력 변수
+    float gravity = -20f;
+    // 수직 속력 변수
+    float yVelocity = 0;
+    // 점프파워
+    public float jumpPower = 7f;
+    // 점프 파워 상태 변수
+    public bool isJumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -26,8 +36,32 @@ public class PlayerMove : MonoBehaviour
         // 카메라를 기준으로 이동
         dir = Camera.main.transform.TransformDirection(dir);
 
+        // 바닥에 착지했다면
+        if (cc.collisionFlags == CollisionFlags.Below && isJumping)
+        {
+            isJumping = false;
+            yVelocity = 0;
+            
+        }
+
+        // 스페이스바 버튼을 누르면 점프
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            yVelocity = jumpPower;
+            isJumping = true;
+            Debug.Log(yVelocity);
+        }
+
         // 이동
-         transform.position += moveSpeed * dir * Time.deltaTime;
+        // transform.position += moveSpeed * dir * Time.deltaTime;
+
+        // 수직 속도에 중력 값 적용
+        yVelocity += gravity * Time.deltaTime;
+        dir.y = yVelocity;
+
+        // 이동
+        cc.Move(dir * moveSpeed * Time.deltaTime);
+        
 
     }
 }
